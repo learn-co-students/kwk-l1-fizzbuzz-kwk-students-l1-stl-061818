9ace87173ca2f0948bd67852ee419458cbc5919c
annaliner <annaliner89@gmail.com>
2018-06-19 18:58:05 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz(x)
+  if x%5==0 && x%3==0
+  puts "fizzbuzz"
+
+  elsif x%3==0 
+  puts "fizz"
+
+  elsif x%5==0
+  puts "buzz"
+end
+end
+
+fizzbuzz (3)
+
+# if a number is divisible by 3 and 5, return "fizzbuzz"
+ # if a number is divisible by 5, return "buzz"
+ # if a number is divisible by 3, return "fizz"
+
